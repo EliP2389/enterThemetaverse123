265882c9dced481c680f4f7faa781abefaa5a496
Eli Pagan <mbk8689@gmail.com>
2022-03-24 16:00:44 -0400

    added moralisprovider tp _app

--- components/Login.js
+++ components/Login.js
@@ -4,10 +4,19 @@ function Login() {
   return (
     <div className="bg-black relative text-white">
         <h1>I am the login screen</h1>
-        <div> 
+        <div className="flex flex-col absolute z-50 h-4/6 w-full
+         items-center justify-center space-y-4"> 
             {/* {Logo} */}
+            <Image 
+            className="object-cover rounded-full" 
+            src='https://wolf-wallpapers.pro/images/2048x1152-wolf-colorful-minimalism-2048x1152-resolution-hd-1-8-wolf-wallpapers.pro.jpg' 
+            height={200} width={200}
+            />
 
             {/* {Login button} */}
+            <button className='bg-red-500 rounder-lg p-5 font-bold animate-pulse'>
+                Login to the METAVERSE
+                </button>
         </div>
 
         <div className="w-full h-screen">
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MoralisProvider } from 'react-moralis'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+    appId="nmBNCXPXtnBXcNzvkYGHv73sJ7WeqoGLLAdz8i2l"
+    serverUrl="https://k7v5moejvt5t.usemoralis.com:2053/server"
+    >
+     <Component {...pageProps} />
+     </MoralisProvider>
+
+  )
 }
 
 export default MyApp
