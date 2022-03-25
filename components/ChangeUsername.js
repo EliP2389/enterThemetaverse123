3fbc7a4408a913e2d5fb3472fae0b06a764369a7
Eli Pagan <mbk8689@gmail.com>
2022-03-25 11:31:42 -0400

    added function to change username

--- components/ChangeUsername.js
+++ components/ChangeUsername.js
@@ -1,7 +1,34 @@
-function ChangeUsername() {
-  return (
-    <div>
+import { useMoralis } from 'react-moralis'
 
+function ChangeUsername() {
+  const { setUserData, isUserUpdating, userError, user } = useMoralis()
+
+  const setUserName = () => {
+      const username = prompt(
+          `Enter your new username (current username: ${user.getUsername()})`
+          )
+
+        // if no username stop function
+          if (!username) return;
+        
+        // changes data in moralis
+          setUserData({
+              username,
+          });
+          console.log(username);
+  };
+
+  
+
+  return (
+    <div className="absolute top-5 right-5 text-sm">
+      <button
+        onClick={setUserName}
+        disabled={isUserUpdating}
+        className="hover:text-rose-700"
+      >
+        Change username
+      </button>
     </div>
   )
 }
