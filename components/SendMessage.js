977267219432c704e7a2715a8f983a95c9937e81
Eli Pagan <mbk8689@gmail.com>
2022-03-26 11:06:56 -0400

    created a Moralis object to beable to save messages to the database

--- components/SendMessage.js
+++ components/SendMessage.js
@@ -5,6 +5,31 @@ function SendMessage() {
   const { user, Moralis } = useMoralis()
   const [message, setMessage]= useState("");
 
+  const sendMessage = (e) => {
+      e.preventDefault();
+
+      if (!message) return;
+
+      const Messages = Moralis.Object.extend('Messages')
+      const messages = new Messages();
+    //  save, saves messages in the table 
+      messages.save({
+          message: message,
+          username: user.getUsername(),
+          ethAddress: user.get('ethAddress'),
+      }).then((message) => {
+          message.json('message was successful')
+      },
+      (error) => {
+        //  the save failed because
+        // error is a Moralis.Error with an error code and Message
+        console.log(error.message);
+      }
+      );
+  };
+
+
+
   return (
     <form className="fixed bottom-10 flex w-11/12 max-w-2xl
      bg-black px-6 py-4 opacity-80 shadow-xl rounded-full border-4 border-purple-500">
@@ -16,7 +41,10 @@ function SendMessage() {
         placeholder={`Enter a Message ${user.getUsername()}...`}
         type="text"
       />
-      <button className="font-bold text-rose-500">Send</button>
+      <button 
+      type="submit" 
+      onClick={setMessage}
+      className="font-bold text-rose-500">Send</button>
     </form>
   )
 }
