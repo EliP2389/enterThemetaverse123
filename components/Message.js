b33da854b85daea000c57829bb9e2fae3b28a11a
Eli Pagan <mbk8689@gmail.com>
2022-03-26 20:32:26 -0400

    added styling to chat box for users

--- components/Message.js
+++ components/Message.js
@@ -1,19 +1,40 @@
-import { useMoralis } from "react-moralis";
+import { useMoralis } from 'react-moralis'
+import Avatar from '../components/Avatar'
 
-function Message({message}) {
-const { user } = useMoralis();
-// determines if the message was sent by user(myself) or someone else
-const isUserMessage = message.get('ethAddress') === user.get('ethAddress');
+function Message({ message }) {
+  const { user } = useMoralis()
+  // determines if the message was sent by user(myself) or someone else
+  const isUserMessage = message.get('ethAddress') === user.get('ethAddress')
 
   return (
-    <div className={`flex items-end space-x-2 relative ${isUserMessage && 'justify-end'}`} >
-        <div className={`flex space-x-4 p-3 rounded-lg 
-        ${isUserMessage 
-            ? "rounded-br-none bg-rose-300"
-            : "rounded-bl-none bg-purple-500"
-        }`}>
-           <p>{message.get('message')}</p> 
-        </div>
+    <div
+      className={`relative flex items-end space-x-2 ${
+        isUserMessage && 'justify-end'
+      }`}
+    >
+      <div className={`relative h-8 w-8 ${isUserMessage && 'order-last ml-2'}`}>
+        <Avatar username={message.get('username')} />
+      </div>
+
+      <div
+        className={`flex space-x-4 rounded-lg p-3 
+        ${
+          isUserMessage
+            ? 'rounded-br-none bg-rose-300'
+            : 'rounded-bl-none bg-purple-500'
+        }`}
+      >
+        <p>{message.get('message')}</p>
+      </div>
+
+      {/* {timestamp} */}
+
+      <p
+        className={`absolute -bottom-5 text-xs
+        ${isUserMessage ? 'text-rose-300' : 'text-purple-500'}`}
+      >
+        {message.get('username')}
+      </p>
     </div>
   )
 }
